a822b7ac02d8dbe35e43f60487fce4741a0d5a5e
Carlos Dz <cdaza@infotransgroup.com>
2022-05-22 11:26:23 -0500

    Se modifica url de health por duplicidad al realizar referencia

--- src/Boostrapers/GeocodingService.Api/Controllers/ApiControllerBase.cs
+++ src/Boostrapers/GeocodingService.Api/Controllers/ApiControllerBase.cs
@@ -7,7 +7,5 @@ namespace GeocodingService.Api.Controllers
     [Route("api/[controller]")]
     public class ApiControllerBase : ControllerBase
     {
-        [HttpGet("/")]
-        public IActionResult Health() => Ok();
     }
 }
---
+++ src/Boostrapers/GeocodingService.Api/Controllers/HealthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeocodingService.Api.Controllers
+{
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet("/")]
+        public IActionResult Health() => Ok();
+    }
+}
